6127ce4f0bf6c1d95863002ce8c22602996e47bb
cosa65 <mfosco65@gmail.com>
2016-09-30 16:38:03 -0300

    dfs anda, falta

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -2,82 +2,81 @@
 #include <vector>
 #include <list>
 #include <stack>
+#include <map>
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-/*class PaintedNode() {
-
-	public:
-	
-	PaintedNode()//int id);
-	PaintedNode(list<int> groups);//, int id);
-	void add_to_group(int new_col);
-	bool in(int group_num);
-	void print_groups();
-
-	private:
-	
-	//int id;
-	list<int> groups;
-};
-
-PaintedNode::PaintedNode(int id) {}
-
-PaintedNode::PaintedNode(int id, list<int> groups) {
-	this.groups = groups;
-}
-
-void PaintedNode::add_to_group(int new_col) {
-	this.groups.push_back(new_col);
-}
-
-bool PaintedNode::in(int group_num) {
-	for (list<int>::iterator it = groups.begin(); it != groups.end(); it++) {
-		if (*it == group_num) return true;
-	}
-	return false;
-}
-
-void PaintedNode::print_groups() {
-	for (list<int>::iterator it = groups.begin(); it != groups.end(); it++) {
-		cout << " " << *it << " ";
-	}
-}*/
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+typedef int Group;
 
 int min(int a, int b) {
-	return (a >= b) * a + (a < b) * b;
+	return (a <= b) * a + (a > b) * b;
 }
 
-void dfs_get_articulations(int curr_node, vector<list<int> >& city, vector<int>& depth, vector<int>& low, vector<int>& parent, int d) {
-
-	for (list<int>::iterator it = city[curr_node].begin(); it != city[curr_node].end(); it++) {
-		depth[*it] = d;
-		low[*it] = d;
+void dfs_get_articulations(int i, vector<list<int> >& la_matris, vector<int>& depth, vector<int>& low, vector<int>& parent, int d, list<int>& articulations) {
+	depth[i] = d;
+	low[i] = d;
+	for (list<int>::iterator it = la_matris[i].begin(); it != la_matris[i].end(); it++) {
 		if (parent[*it] == -1) {
-			parent[*it] = curr_node;
-			dfs_get_articulations(*it, city, depth, low, parent, d+1);
-			++d;
-		} else if(parent[curr_node] != *it) {
-			low[curr_node] = min(low[curr_node], depth[*it]);
+			parent[*it] = i;
+			dfs_get_articulations(*it, la_matris, depth, low, parent, d+1, articulations);
+			if (low[*it] >= depth[i] and i != 0) articulations.push_back(i);
+			low[i] = min(low[i], low[*it]);
+		} else if (parent[i] != *it) {
+			low[i] = min(low[i], depth[*it]);
 		}
 	}
+
 }
 
-void create_block_cut_tree(vector<list<int> >& city) {
+void create_block_cut_tree(vector<list<int> >& la_matris) {
 
-	vector<int> depth(city.size());
-	vector<int> low(city.size());
-	vector<int> parent(city.size(), -1);
-	int curr_node = 0;
+	vector<int> depth(la_matris.size());
+	list<int> articulations;
+	vector<int> low(la_matris.size());
+	vector<int> parent(la_matris.size(), -1);
+	int i = 0;
 	depth[0] = 0;
-	//parent[0] = 0;
+	parent[0] = 0;
 	low[0] = 0;
-	int d = 0;
-	dfs_get_articulations(curr_node, city, depth, low, parent, d);
+	dfs_get_articulations(i, la_matris, depth, low, parent, 0, articulations);
+
+	bool cambio = false;
+	int childLow = low[la_matris[0].front()];
+	for (list<int>::iterator it = la_matris[0].begin(); it != la_matris[0].end(); it++) {
+		if (low[*it] != childLow) {
+			cambio = true;
+			break;
+		}
+	}
+
+	if (cambio) articulations.push_back(0);
+
+	cout << "Puntos de articulacion: ";
+	for (list<int>::iterator it = articulations.begin(); it != articulations.end(); it++) {
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
+map<Group, int> get_groups(vector<list<int> >& la_matris, list<int>& articulations) {
+	map<Group, int> res;
+	int N = la_matris.size();
+	for (int i = 0 ; i < N; i++) {
+		res.emplace(i, low[i]);
+	}
+	return res;
+}
+
+int query_A() {
+	return -1;
+}
+
+int query_B() {
+	return -1;
+}
+
+int query_C() {
+	return -1;	
 }
 
 int main() {
@@ -94,6 +93,8 @@ int main() {
 		la_matris[e1].push_back(e2);
 		la_matris[e2].push_back(e1);
 	}
+	create_block_cut_tree(la_matris);
+	get_groups(la_matris, articulations);
 
 	return 0;
 }
