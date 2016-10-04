b4171707661411330a047b06b4d64dfb21af99f4
cosa65 <mfosco65@gmail.com>
2016-10-03 21:09:35 -0300

    lucas me esta apurando mucho

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -22,6 +22,18 @@ int min(int a, int b) {
 	return (a <= b) * a + (a > b) * b;
 }
 
+void agregar_si_no_esta(list<int>& ls, int num) {
+	bool esta = false;
+
+	for (list<int>::iterator it = ls.begin(); it != ls.end(); it++) {
+		if (*it == num) esta = true;
+	}
+
+	if (not esta) {
+		ls.push_back(num);
+	}
+}
+
 struct Edge {
 
 	Edge(int v, int w) {
@@ -54,13 +66,20 @@ public:
 	void add_new_component(Edge e);
 	void add_last_component();
 	void create_block_cut_tree();
+	int get_unvisited_node(vector<bool> visited);
+	void save_component_size(list<int> in_component, int res);
+	void start_dfs_without_bridges();
+	int dfs_without_bridges(int d, vector<bool>& visited, list<int>& nodes_in_component);
 	bool bridge(Edge e);
 	void print_components();
 	void print_block_cut_tree();
 	void print_components_of_nodes();
+	void print_components_sizes();
 	int query_A(int e1, int e2);
 	int query_B(int calle);
 	int query_C(int e1);
+	void solve_queries();
+
 
 private:
 
@@ -71,11 +90,13 @@ private:
 	vector<int> parent;
 	vector<bool> articulation;
 	vector<int> component;
-	list<list<Edge> > components;
-	vector<int> components_sizes;
+	list<list<Edge> > components_edges;
+	vector<int> blocks_cuts_sizes;
 	stack<Edge> members_of_component;
 	int components_num;
 	vector<Edge> calles;
+	vector<int> results_queryC;
+	vector<int> connected_components_sizes;
 };
 
 BlockGraphProcessor::BlockGraphProcessor(vector<list<int> >& la_matris, vector<Edge> calles) {
@@ -88,6 +109,8 @@ BlockGraphProcessor::BlockGraphProcessor(vector<list<int> >& la_matris, vector<E
 	parent.resize(la_matris.size(), -1);
 	articulation.resize(la_matris.size(), false);
 	component.resize(la_matris.size(), -1);
+	results_queryC.resize(la_matris.size(), 0);
+	connected_components_sizes.resize(la_matris.size());
 	components_num = 0;
 }
 
@@ -97,6 +120,7 @@ void BlockGraphProcessor::dfs_set_articulations(int i, int d) {
 	if(parent[i] != i) {
 		members_of_component.push(Edge(i, parent[i]));
 	}
+
 	for (list<int>::iterator it = la_matris[i].begin(); it != la_matris[i].end(); it++) {
 		if (parent[*it] == -1) {
 			parent[*it] = i;
@@ -133,7 +157,6 @@ void BlockGraphProcessor::process_articulation_points() {
 	add_last_component();			//Agrego al componente del nodo raiz, 
 									//que no lo incluye en el proceso normal porque es un caso borde.
 
-
 	cout << "Puntos de articulacion: ";
 	int N = articulation.size();
 	for (i = 0; i < N; i++) {
@@ -144,7 +167,7 @@ void BlockGraphProcessor::process_articulation_points() {
 	for(i = 0; i < N; i++) {		//Ahora genero componentes de cada punto de articulacion
 		if(articulation[i]) {
 			component[i] = components_num;
-			components_sizes.push_back(1);
+			blocks_cuts_sizes.push_back(1);
 			components_num++;
 		}
 	}
@@ -165,7 +188,7 @@ void BlockGraphProcessor::add_new_component(Edge e) {
 			component[top.w] = components_num;
 		}
 		members_of_component.pop();
-		components.push_back(new_component);
+		components_edges.push_back(new_component);
 		components_num++;
 //	} else {
 //		members_of_component.pop();
@@ -182,7 +205,7 @@ void BlockGraphProcessor::add_last_component() {
 	}
 	if (not new_component.empty()) {
 		components_num++;
-		components.push_back(new_component);
+		components_edges.push_back(new_component);
 	}
 }
 
@@ -191,9 +214,9 @@ void BlockGraphProcessor::create_block_cut_tree() {
 	int component_of_v;
 	int component_of_w;
 	block_cut_tree.resize(components_num);
-	for(list<list<Edge> >::iterator iti = components.begin(); iti != components.end(); iti++) {
+	for(list<list<Edge> >::iterator iti = components_edges.begin(); iti != components_edges.end(); iti++) {
 		list<int> edges_of_new_node;
-		cout << "Estoy con el componente " << i << endl;
+		//cout << "Estoy con el componente " << i << endl;
 		for(list<Edge>::iterator itj = iti->begin(); itj != iti->end(); itj++) {
 			if(articulation[itj->v] or articulation[itj->w]) {
 				component_of_v = component[itj->v];
@@ -201,11 +224,53 @@ void BlockGraphProcessor::create_block_cut_tree() {
 				block_cut_tree[component_of_v].push_back(component_of_w);
 				block_cut_tree[component_of_w].push_back(component_of_v);
 			} else {
-				components_sizes[component_of_v]+=2;
+				blocks_cuts_sizes[component_of_v]+=2;
 			}
 		}
 		i++;
 	}
+}
+
+int BlockGraphProcessor::get_unvisited_node(vector<bool> visited) {
+	int N = la_matris.size();
+	for (int i = 0; i < N; ++i) {
+		if (not visited[i]) {
+			return i;
+		}
+	}
+}
+
+void BlockGraphProcessor::save_component_size(list<int> nodes_in_component, int size) {
+	for (list<int>::iterator it = nodes_in_component.begin(); it != nodes_in_component.end(); it++) {
+		connected_components_sizes[*it] = size;
+	}
+}
+
+void BlockGraphProcessor::start_dfs_without_bridges() {
+	int size;
+	int N = la_matris.size();
+	vector<bool> visited(N, false);
+	list<int> nodes_in_component(N);
+	int next_d = get_unvisited_node(visited);
+
+	while (next_d != -1) {
+		size = dfs_without_bridges(next_d, visited, nodes_in_component);
+		save_component_size(nodes_in_component, size);
+		next_d = get_unvisited_node(visited);
+	}
+}
+
+int BlockGraphProcessor::dfs_without_bridges(int d, vector<bool>& visited, list<int>& nodes_in_component) {
+	int N = la_matris.size();
+	visited[d] = true;
+	int amount_in_component = 0;
+	for (list<int>::iterator it = la_matris[d].begin(); it != la_matris[d].end(); it++) {
+		if ( (not bridge(Edge(*it, d))) and (not visited[*it]) ) {
+			amount_in_component += dfs_without_bridges(*it, visited, nodes_in_component);
+		}	
+	}
+	nodes_in_component.push_back(d);
+	return amount_in_component + 1;
 }
 
 bool BlockGraphProcessor::bridge(Edge e) {
@@ -214,7 +279,7 @@ bool BlockGraphProcessor::bridge(Edge e) {
 
 void BlockGraphProcessor::print_components() {
 	int i = 0;
-	for(list<list<Edge> >::iterator iti = components.begin(); iti != components.end(); iti++) {
+	for(list<list<Edge> >::iterator iti = components_edges.begin(); iti != components_edges.end(); iti++) {
 		cout << "Componente " << i << endl;
 		for(list<Edge>::iterator itj = iti->begin(); itj != iti->end(); itj++) {
 			cout << *itj << " ";
@@ -242,8 +307,15 @@ void BlockGraphProcessor::print_components_of_nodes() {
 	}
 }
 
+void BlockGraphProcessor::print_components_sizes() {
+	int N = blocks_cuts_sizes.size();
+	for(int i = 0; i < N; i++) {
+		cout << "Component " << i << " size " << blocks_cuts_sizes[i] << endl;
+	}
+}
+
 int BlockGraphProcessor::query_A(int e1, int e2) {
-return -1;
+	return -1;
 }
 
 int BlockGraphProcessor::query_B(int num_calle) {
@@ -256,7 +328,49 @@ int BlockGraphProcessor::query_B(int num_calle) {
 }
 
 int BlockGraphProcessor::query_C(int e1) {
-	return components_sizes[e1]-1;
+	return connected_components_sizes[e1];
+}
+
+void BlockGraphProcessor::solve_queries() {
+	int Q, arg;
+	cin >> Q;
+	vector<int> results;
+	vector<int> inputs;
+	list<char> query_types;
+	char query_type;
+
+	for (int i = 0; i < Q; i++) {			//Recibiendo input de queries
+		cin >> query_type;
+		if (query_type == 'A') {
+			cin >> arg;
+			inputs.push_back(arg);
+		}
+		cin >> arg;
+		inputs.push_back(arg);
+		query_types.push_back(query_type);
+	}
+
+	int i = 0;
+
+	for (list<char>::iterator it = query_types.begin(); it != query_types.end(); it++) {				//Procesando queries y guardando resultados
+		switch (*it) {
+			case 'A':
+				results.push_back(query_A(inputs[i], inputs[i + 1]));
+				++i;
+				break;
+			case 'B':
+				results.push_back(query_B(inputs[i]));
+				break;
+			case 'C':
+				results.push_back(query_C(inputs[i]));
+				break;
+		}
+		++i;
+	}
+
+	for (i = 0; i < Q; ++i) {
+		cout << results[i] << endl;
+	}
 }
 
 int main() {
@@ -278,10 +392,12 @@ int main() {
 	
 	BlockGraphProcessor solver(la_matris, calles);
 	solver.process_articulation_points();
-	solver.print_components();
+	//solver.print_components();
 	solver.create_block_cut_tree();
-	solver.print_block_cut_tree();
-	solver.print_components_of_nodes();
+//	solver.print_block_cut_tree();
+//	solver.print_components_of_nodes();
+	solver.solve_queries();
+	solver.print_components_sizes();
 
 	return 0;
 }
